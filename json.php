c2c383b516b24820ea19ed19f813d9672f8cd94b
cynthiamd <cynthiamicheld@gmail.com>
2016-05-24 15:55:43 +0200

    rr

--- json.php
+++ json.php
@@ -1,10 +1,10 @@
 <?php
-​
+
 // File that stores all the JSON data
-$filename = "http://ddwap.mah.se/ae4200/teamsmart/settings.json";
-​
+$filename = "settings.json";
+
 $requestMethod = $_SERVER["REQUEST_METHOD"];
-​
+
 if ($requestMethod == "GET") {
     // File doesnt exist
     if (!file_exists($filename)) {
@@ -21,13 +21,15 @@ if ($requestMethod == "GET") {
     // Stop the script from continuing
     exit(0);
 }
-​
+
 if ($requestMethod == "POST") {
     // Decode the JSON data we got
     $json = json_decode(file_get_contents('php://input'));
-​
+
     // Store everything in our file
     file_put_contents($filename, json_encode($json));
 
     exit(0);
 }
+
+?>
