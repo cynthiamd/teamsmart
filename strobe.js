a62373e7ec6f10afc31414eceb3c924ccc054a7c
talheiman <kristian9309@gmail.com>
2016-05-31 14:14:23 +0200

    comment the code

--- strobe.js
+++ strobe.js
@@ -1,12 +1,9 @@
-//MAN = function 1 - dbl - nightmode
-//MAN = function 2 - wakt up, press 1 time enter daymode. (shuts off all light) - buttndown.
-//MAN = function 3 - set all light standard colors -
-//MAN = function 4 - panick mode. - hold button
 //Require johnny-five, arduino board and the buttons
 var five = require("johnny-five"),
     board, button1, button2;
 //Create a new board
 board = new five.Board();
+//global variable for the philips hue settings
 var on;
 var sat;
 var hue;
@@ -41,6 +38,7 @@ board.on("ready", function() {
     // Create a new `button` hardware instance.
     // This example allows the button module to
     // create a completely default instance
+    //holdtime is the timer for how long you hold down the button.
     button1 = new five.Button({
         pin: 2,
         holdtime: 1000
@@ -49,12 +47,12 @@ board.on("ready", function() {
         pin: 3,
         holdtime: 1000
     });
-    //creates a pizo obj and defines the pin to be used for the signal
+    //creates a piezo obj and defines the pin to be used for the signal
     var piezo = new five.Piezo(5);
     board.repl.inject({
         piezo: piezo
     });
-
+    // make an request to get the settings file and save the data from the file into the variable settings.
     function getJSONfile() {
             request('http://xn--paulinehgh-lcb.se/smarthome/json.php',
                 function(error, response, body) {
@@ -64,9 +62,10 @@ board.on("ready", function() {
                     }
                 });
         }
-        //Controlling the net connection every 10 seconds
+    // run the controllConnection function every 1 second.
     setInterval(controllConnection, 1000);
-
+    //Ping the host of the ip adress and if there is an error play a beep sound through the piezo.
+    //otherwise fetch the light from philips hue api and get the file containing all the settings for the lamps.
     function controllConnection() {
         session.pingHost(ipAdress, function(err, t) {
             if (err) {
@@ -84,13 +83,16 @@ board.on("ready", function() {
             }
         });
     }
-
+    //This function makes an post request to philips hue api and if there is an problem, beep sound come off from the
+    // piezo. otherwise make a new request to philips hue and get the lamps and save them into an array.
     function fetchLights() {
+            //sends an request to philips hue api.
             request({
                 method: 'POST',
                 url: HueApi
             }, function(err, res, body) {
                 if (err) {
+                  //plays a beep sound through piezo
                     piezo.play({
                         song: "C C C C",
                         beats: 1 / 4,
@@ -99,7 +101,6 @@ board.on("ready", function() {
                     console.log('error', err);
                     return;
                 }
-                //      console.log('search for lights', body);
                 request(HueApi, function(err, res, body) {
                     if (err) {
                         piezo.play({
@@ -110,17 +111,14 @@ board.on("ready", function() {
                         console.log('error', err);
                         return;
                     }
-                    //    console.log('get new lights', body);
                     var json = JSON.parse(body);
                     for (lamp in json) {
                         lamps.push(json[lamp]);
                     }
-                    //    console.log(lamps);
                 });
             });
         }
-        //Function for changing light on Hue!
-
+    //This function changes the lamps color by making a request to philips hue api.
     function changeColor(lamp, statement) {
         request({
             method: "PUT",
@@ -128,12 +126,11 @@ board.on("ready", function() {
             json: statement
         });
     }
-
+    //This function will turnoff the lamps by looking at the settings from the settings file.
     function turnOff(lamp) {
-            settings.dayMode.lights.forEach(function(
-                light) {
-                changeColor("/lights/" + light.id
-                    .substr(-1) + "/state", {
+            settings.dayMode.lights.forEach(function(light) {
+                changeColor("/lights/" + light.id.substr(-1) +
+                    "/state", {
                         on: light.on,
                         sat: light.sat,
                         bri: light.bri,
@@ -141,9 +138,9 @@ board.on("ready", function() {
                     });
             });
         }
-        //Set Daymode with 60sek interval
+    //Set Daymode and it will run every 60 seconds
     setInterval(dayMode, 60000);
-
+    //This function activates day mode and shuts off the lights at a specified time.
     function dayMode() {
             //Get current date in milliseconds
             var now = new Date();
@@ -153,7 +150,8 @@ board.on("ready", function() {
             var day = now.getDay();
             //Check if its a weekday else its weekend
             if (day !== 0 && day !== 6) {
-                if (now.getHours() == settings.dayMode.hours && now.getMinutes() == settings.dayMode.minute) {
+                if (now.getHours() == settings.dayMode.hours && now.getMinutes() ==
+                    settings.dayMode.minute) {
                     if (!foundBridge) {
                         console.log('bridge doesnt exist');
                         return;
@@ -165,7 +163,8 @@ board.on("ready", function() {
                     console.log("Daymode");
                 }
             } else {
-                if (now.getHours() == settings.dayMode.hours && now.getMinutes() == settings.dayMode.minute) {
+                if (now.getHours() == settings.dayMode.hours && now.getMinutes() ==
+                    settings.dayMode.minute) {
                     if (!foundBridge) {
                         console.log('bridge doesnt exist');
                         return;
@@ -178,15 +177,20 @@ board.on("ready", function() {
                 }
             }
         }
-        //When presses down, one click
+    //button1 functions
     button1.on("hold", buttonPanic);
     button1.on("down", buttonDownMultiClick);
     button1.on("up", buttonUp);
+
+    //button2 functions
     button2.on("down", buttonTwoDown);
     button2.on("hold", buttonPanic);
     button2.on("up", buttonUp);
-    //When you press buttonDown,  change color to all three lamps
+
+    //buttonpanic will make all the lamps turn into a red color and starts blinking for around 30 seconds.
+    //at the same time a beep sound will be played throuhg the piezo speaker.
     function buttonPanic() {
+      //checks if there is a conenctions to the bridge, if we got the settigns.No error execute the panicmode button.
         if (!foundBridge) {
             console.log('bridge doesnt exist');
             return;
@@ -195,10 +199,10 @@ board.on("ready", function() {
             return false;
         } else if (isHoldingDown == false) {
             isHoldingDown = true;
-            clearTimeout(singelTimer);
-            count = 0;
-            var alert = "lselect";
-            settings.panicMode.lights.forEach(function(light) {
+            clearTimeout(singelTimer);// clear the timer
+            count = 0; // resets count to 0.
+            var alert = "lselect"; // a property for philips hue api.
+            settings.panicMode.lights.forEach(function(light) { // for each lamp change to this kind of settings
                 changeColor("/lights/" + light.id.substr(-1) +
                     "/state", {
                         on: light.on,
@@ -209,20 +213,22 @@ board.on("ready", function() {
                     });
             });
             console.log("Hold Mode");
-            /*    interval = setInterval(function() {
-              piezo.play({
-                  song: "C C C C",
-                  beats: 1 / 4,
-                  tempo: 100
-              });
-          }, 3000);
-          setTimeout(function() {
-              clearInterval(interval);
-          }, 20000);*/
+            // plays the beep sound in a iterval every 3 seconds for 20 seconds
+            interval = setInterval(function() {
+                piezo.play({
+                    song: "C C C C",
+                    beats: 1 / 4,
+                    tempo: 100
+                });
+            }, 3000);
+            setTimeout(function() {
+                clearInterval(interval);
+            }, 20000);
         }
     }
-
+    //This function have multiple clicks actions. Single click activates daymode, dbl click ngiht mode and triple standard mode.
     function buttonDownMultiClick() {
+      //checks if there is a bridge connection
             if (!foundBridge) {
                 console.log('bridge doesnt exist');
                 return;
@@ -235,19 +241,24 @@ board.on("ready", function() {
                 singelTimer = setTimeout(function() {
                     //Count 0
                     count = 0;
+                    //call daymode function
                     dayMode();
-                    console.log("one click");
+                    console.log("one click = Day mode");
                 }, timerOneclick);
                 //Else if clicks = 2
             } else if (count === 2) {
-                //Change color on lamps
+              //checks if we got the settings
                 if (!settings) {
                     console.log('settings har inte hämtats');
                     return false;
                 }
+                //clear the timeout for singletimer
                 clearTimeout(singelTimer);
+                //set a timout for doubletimer
                 doubleTimer = setTimeout(function() {
+                  // sets count to 0
                     count = 0;
+                    // for each lamp change according to the settings
                     settings.nightMode.lights.forEach(function(
                         light) {
                         changeColor("/lights/" + light.id
@@ -258,7 +269,7 @@ board.on("ready", function() {
                                 hue: light.hue
                             });
                     });
-                    console.log("två klick");
+                    console.log("två klick = night mode");
                 }, timerDblClick);
             } else if (count === 3) {
                 if (!settings) {
@@ -275,19 +286,18 @@ board.on("ready", function() {
                             hue: light.hue
                         });
                 });
+                //clear timout for doubletimer
                 clearTimeout(doubleTimer);
+                // clear the interval for variable interval
                 clearInterval(interval);
-                console.log("tre klick");
+                console.log("tre klick - standard mode");
             }
         }
-        // When you hold down the button for a certain time, change the color.
-        //When button is up, change the button state to false.
-
+    //????
     function buttonUp() {
             isHoldingDown = false;
         }
-        // buttonTwoDown, change all three lamps to same color when pressed down
-
+    //This function will set the lamps to awaymode on double click and standard on triple click.
     function buttonTwoDown() {
         if (!foundBridge) {
             console.log('bridge doesnt exist');
@@ -302,11 +312,15 @@ board.on("ready", function() {
                 console.log("One Click Button two");
             }, timerOneclick);
         } else if (count === 2) {
+            // cleartimeout for signletimer
             clearTimeout(singelTimer);
             doubleTimer = setTimeout(function() {
+                // sets count to 0
                 count = 0;
+                //execute awaymode
                 awayMode();
-                setInterval(awayMode, 60*10*1000);
+                //awaymode should run every 10 minutes when activated
+                setInterval(awayMode, 60 * 10 * 1000);
             }, timerDblClick);
         } else if (count === 3) {
             count = 0;
@@ -334,19 +348,27 @@ board.on("ready", function() {
             console.log('settings har inte hämtats');
             return false;
         }
+        //obj variable gets the cycle array from the settings file
         var obj = settings.awayMode.cycle;
+        //creates empty array
         var arr = [];
+        // save the result of lights and gethours to variabe ljus
         var ljus = "lights" + nu.getHours();
+        //loop through all the property in obj
         for (var prop in obj) {
+          //push the properties into a array
             arr.push(prop);
         }
+        //loop through the array
         for (var i = 0; i < arr.length; i++) {
+          //if the array contains anythings thats the same as ljus
             if (arr[i] == ljus) {
-                var lol = arr[i];
+              //save it to lol
+                var lightsForaway = arr[i];
                 console.log(lol);
             }
         }
-        settings.awayMode.cycle[lol].forEach(function(light) {
+        settings.awayMode.cycle[lightsForaway].forEach(function(light) {
             changeColor("/lights/" + light.id.substr(-1) +
                 "/state", {
                     on: light.on,
