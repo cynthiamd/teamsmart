c388e301fdd18dcd61cfc28d952b1c57a0db6048
flandans <anders.andersson@ymail.com>
2016-05-31 10:15:27 +0200

    update

--- main.js
+++ main.js
@@ -1,49 +1,51 @@
-/* 
+/*
  * API URL for Philips Hue
  */
 var hueURL =
     "http://192.168.10.247/api/28dd08062078de67270d8b6ab5b3f9b";
-/* 
+/*
  * Partial API for Philips lights
- */	
+ */
 var lightsURL = "/lights/";
-/* 
+/*
  * API URL for retreiving light mode settings
  */
 var jsonURL = "http://xn--paulinehgh-lcb.se/smarthome/json.php";
 
-/* 
+/*
  * Lamp nbr 1 = Bedroom
  */
 var lamp1 = "/lights/1/state";
-/* 
+/*
  * Lamp nbr 2 = Livingroom
  */
 var lamp2 = "/lights/2/state";
-/* 
+/*
  * Lamp nbr 3 = Hallway
  */
 var lamp3 = "/lights/3/state";
-/* 
+
+var lamps = [];
+/*
  * Initial settings value. Valiable holds settings for all modes and lams.
  */
 var settings = false;
-/* 
+/*
  * Inital value for the arduino button, "hold".
  */
 var isHoldingDown = false;
-/* 
+/*
  * Variable for the arduino to set a specific time interval for daymode.
  */
 var interval;
 /*
  * Function is called from....
  */
-function getColorInputs(lights)  {
-    return lights.map(function(light) {
-        return '<span>' + light.id + '</span><input name="' + light.id + '" class="colorPicker jscolor {mode:\'HSV\',position:\'right\'}">';
-    }).join("");
-}
+ function getColorInputs(lights, mode)  {
+     return lights.map(function(light) {
+         return '<span>' + light.id + '</span><input id="' + mode + '-' + light.id + '" name="' + light.id + '" class="colorPicker jscolor">';
+     }).join("");
+ }
 /*
  * Retrive light mode settings from file and assign values to the settings variable.
  */
@@ -59,14 +61,61 @@ function getSettings() {
     });
 }
 /*
- * Duplicate? 
+ * Duplicate?
  */
+
+
+
 $.getJSON(jsonURL, function(json) {
     settings = json;
+    setupColorInputFields(settings);
+
 });
 
+function saveSettings(settings) {
+    $.ajax({
+        url: jsonURL,
+        type: 'POST',
+        data: JSON.stringify(settings),
+        success: function(res) {
+            console.log('stored new settings?');
+        }
+    });
+}
+
+function setupColorInputFields(settings) {
+    // Night Mode
+    var inputsForNightMode = getColorInputs(settings.nightMode.lights, 'nightMode');
+    $("#nightMode").prepend(inputsForNightMode);
+
+
+    // activate color inputs
+    jscolor.installByClassName("jscolor");
+
+    // set default color of input fields
+    settings.nightMode.lights.forEach(function(light) {
+        var hsv = lightHueToHSV(light);
+        document.getElementById("nightMode-" + light.id).jscolor.fromHSV(hsv[0], hsv[1], hsv[2]);
+    });
+}
+
+function HSVtoHue(hsv) {
+    return {
+        sat: Math.round((hsv[2] * 256) / 100),
+        bri: Math.round((hsv[1] * 256) / 100),
+        hue: Math.round((hsv[0] * 65536) / 360)
+    };
+}
+
+function lightHueToHSV(light) {
+    return [
+        Math.round((light.hue / 65536) * 360),
+        Math.round((light.bri / 256) * 100),
+        Math.round((light.sat / 256) * 100)
+    ];
+}
 /*
- * Connect to Philips Hue bridge. 
+ * Connect to Philips Hue bridge.
  */
 $(document).ready(function() {
 
@@ -80,9 +129,9 @@ $(document).ready(function() {
         }
     });
 
-	/*
-	 * Connect to Philips Hue lighs in the bridge to search for a lamp.
-	*/
+    /*
+     * Connect to Philips Hue lighs in the bridge to search for a lamp.
+     */
     function searchLamps(id) {
         $.ajax({
             url: hueURL + lightsURL,
@@ -94,9 +143,9 @@ $(document).ready(function() {
             }
         });
     }
-	/*
-	 * Connect to Philips Hue lighs in the bridge to retreive a lamp.
-	 */
+    /*
+     * Connect to Philips Hue lighs in the bridge to retreive a lamp.
+     */
     function getLamps() {
         $.ajax({
             url: hueURL + lightsURL,
@@ -108,11 +157,11 @@ $(document).ready(function() {
         });
     }
 
-	/*
-	 * Connect to Philips Hue lighs connected to the bridge, to retreive a lamp.
-	 * On succes, new state is returned in the response.
-	 * Pending jibberish to be removed? 9438c4b66bb9646f80f8dc3470356264ad444aa8
-	 */
+    /*
+     * Connect to Philips Hue lighs connected to the bridge, to retreive a lamp.
+     * On succes, new state is returned in the response.
+     * Pending jibberish to be removed? 9438c4b66bb9646f80f8dc3470356264ad444aa8
+     */
     function changeColor(lamp, statement) {
 
         $.ajax({
@@ -128,11 +177,11 @@ $(document).ready(function() {
         });
     }
 
-	/*
-	 * Philips Hue call to update light state to turn off a lamp.
-	 * On succes, new state is returned in the response.
-	 * Current implementation does not use this function.
-	 */
+    /*
+     * Philips Hue call to update light state to turn off a lamp.
+     * On succes, new state is returned in the response.
+     * Current implementation does not use this function.
+     */
     function turnOff(lamp) {
 
         var statement = {
@@ -150,10 +199,10 @@ $(document).ready(function() {
             }
         });
     }
-	/*
-	 * Philips Hue call to update light state for a lamp to toggle on off (blink).
-	 * On succes, new state is received.
-	 */
+    /*
+     * Philips Hue call to update light state for a lamp to toggle on off (blink).
+     * On succes, new state is received.
+     */
     function alert(lamp) {
         var statement = {
             "on": true,
@@ -175,27 +224,52 @@ $(document).ready(function() {
 
 
     /*
-	 * Activate daymode according to interval.
-	 */
+     * Activate daymode according to interval.
+     */
     setInterval(dayMode, 60000);
 
-	/*
-	 * Day mode.
-	 * When daymode is active, corresponding settingse are 
-	 * active at different hours depending on if it is a weekday or weekend.
-	 */
+    /*
+     * Day mode.
+     * When daymode is active, corresponding settingse are
+     * active at different hours depending on if it is a weekday or weekend.
+     */
+    $("#dayMode").submit(function(e) {
+        e.preventDefault();
+
+        var hour = e.target.elements.hour.value;
+        var minute = e.target.elements.minute.value;
+
+        console.log('settings before change', settings.dayMode);
+        settings.dayMode.hours = hour;
+        settings.dayMode.minute = minute;
+        console.log('settings after change', settings.dayMode);
+        // dayMode(hour, minute);
+        saveSettings(settings);
+    });
+
+
     function dayMode() {
+
 
         if (!settings) {
             console.log('settings har inte hämtats');
             return false;
         }
+
+        //Get current date in milliseconds
         var now = new Date();
+        //Get current time 10:28
         var timefor = now.getHours() + ":" + now.getMinutes();
+        //Get current weekday number 0-6
         var day = now.getDay();
-       
+        //Check if its a weekday else its weekend
         if (day !== 0 && day !== 6) {
-            if (now.getHours() == 13 && now.getMinutes() == 36) {
+            if (now.getHours() == settings.dayMode.hours && now.getMinutes() == settings.dayMode.minute) {
+                //Turn off lamps
+                //turnOff(lamp1);
+                //turnOff(lamp2);
+                //turnOff(lamp3);
+
                 settings.dayMode.lights.forEach(function(light) {
                     changeColor("/lights/" + light.id.substr(-1) + "/state", {
                         on: light.on,
@@ -207,7 +281,13 @@ $(document).ready(function() {
                 console.log("Daymode weekday");
             }
         } else {
-            if (now.getHours() == 10 && now.getMinutes() == 15) {
+            if (now.getHours() == settings.dayMode.hours && now.getMinutes() == settings.dayMode.minute) {
+
+                //Turn off lamps
+                //turnOff(lamp1);
+                //turnOff(lamp2);
+                //turnOff(lamp3);
+
                 settings.dayMode.lights.forEach(function(light) {
                     changeColor("/lights/" + light.id.substr(-1) + "/state", {
                         on: light.on,
@@ -221,10 +301,10 @@ $(document).ready(function() {
         }
     }
 
-	/*
-	 * Standard mode.
-	 * Currently not implemented for the webb application.
-	 */
+    /*
+     * Standard mode.
+     * Currently not implemented for the webb application.
+     */
     function standard() {
         if (!settings) {
             console.log('settings har inte hämtats');
@@ -240,12 +320,12 @@ $(document).ready(function() {
         });
         clearInterval(interval);
     }
-	/*
-	 * Night mode.
-	 * Changes lights state according to settings.
-	 */
+    /*
+     * Night mode.
+     * Changes lights state according to settings.
+     */
     function nightMode() {
-        
+
         if (!settings) {
             console.log('settings har inte hämtats');
             return false;
@@ -262,10 +342,10 @@ $(document).ready(function() {
         console.log("två klick");
     }
 
-	/*
-	 * Away mode.
-	 * Changes lights state every hour according to settings shedule.
-	 */
+    /*
+     * Away mode.
+     * Changes lights state every hour according to settings shedule.
+     */
     function awayMode() {
         var nu = new Date();
         var tid = nu.getHours();
@@ -276,7 +356,7 @@ $(document).ready(function() {
         }
 
         var ljus = "lights" + nu.getHours();
-		
+
         window["settings"]["awayMode"]["cycle"][ljus].forEach(function(light) {
             changeColor("/lights/" + light.id.substr(-1) + "/state", {
                 on: light.on,
@@ -291,44 +371,73 @@ $(document).ready(function() {
     setInterval(awayMode, 600000);
 
 
-	/*
-	 * Wake up mode.
-	 * When wake up is active, corresponding settingse are 
-	 * active at different hours depending on if it is a weekday or weekend.
-	 * Wake up light toggles on off every minute.
-	 */
+    /*
+     * Wake up mode.
+     * When wake up is active, corresponding settingse are
+     * active at different hours depending on if it is a weekday or weekend.
+     * Wake up light toggles on off every minute.
+     */
 
     setInterval(wakeUp, 60000);
 
+    $("#wakeUp").submit(function(e) {
+        e.preventDefault();
+
+        var hour = e.target.elements.hour.value;
+        var minute = e.target.elements.minute.value;
+
+        console.log('settings before change', settings.wakeUp);
+        settings.wakeUp.hours = hour;
+        settings.wakeUp.minute = minute;
+        console.log('settings after change', settings.wakeUp);
+        // dayMode(hour, minute);
+        saveSettings(settings);
+    });
+
+
     function wakeUp() {
-        
+        //Get current date in milliseconds
         var now = new Date();
+        //Get current time 10:28
         var timefor = now.getHours() + ":" + now.getMinutes();
         //Get current weekday number 0-6
         var day = now.getDay();
         //Check if its a weekday else its weekend
         if (day !== 0 && day !== 6) {
-            if (now.getHours() == 11 && now.getMinutes() == 22) {
+            if (now.getHours() == settings.wakeUp.hours && now.getMinutes() == settings.wakeUp.minute) {
                 //Alert lamps
-                alert(lamp2);
-                console.log("Wake Up weekday");
-
+                settings.wakeUp.lights.forEach(function(light) {
+                    changeColor("/lights/" + light.id.substr(-1) + "/state", {
+                        on: light.on,
+                        sat: light.sat,
+                        bri: light.bri,
+                        hue: light.hue
+                    });
+                });
+                console.log("weekday");
             }
         } else {
-            if (now.getHours() == 10 && now.getMinutes() == 15) {
+            if (now.getHours() == settings.wakeUp.hours && now.getMinutes() == settings.wakeUp.minute) {
 
-                alert(lamp2);
-                console.log("Wake Up weekends");
+                settings.wakeUp.lights.forEach(function(light) {
+                    changeColor("/lights/" + light.id.substr(-1) + "/state", {
+                        on: light.on,
+                        sat: light.sat,
+                        bri: light.bri,
+                        hue: light.hue
+                    });
+                });
+                console.log("Weekend");
             }
 
 
         }
 
     }
-	/*
-	 * Panic mode.
-	 * Lamps light toggles on off every minute for a duration of 20000 ms.
-	 */
+    /*
+     * Panic mode.
+     * Lamps light toggles on off every minute for a duration of 20000 ms.
+     */
 
     function panicMode() {
         if (!settings) {
@@ -350,18 +459,18 @@ $(document).ready(function() {
         alert(lamp2);
         alert(lamp3);
 
-        interval = setInterval(function () {
-          var audiofile = document.getElementById('audio');
-          audiofile.play();
+        interval = setInterval(function() {
+            var audiofile = document.getElementById('audio');
+            audiofile.play();
         }, 2000);
         setTimeout(function() {
             clearInterval(interval);
         }, 20000);
     }
-	
-	/*
-	 * Adding listeners for the light mode icons.
-	 */
+
+    /*
+     * Adding listeners for the light mode icons.
+     */
     $("#daymode").click(function() {
         dayMode();
     });
@@ -386,9 +495,9 @@ $(document).ready(function() {
         panicMode();
     });
 
-	/*
-	 * Adding listeners for the show slidebar class.
-	 */
+    /*
+     * Adding listeners for the show slidebar class.
+     */
     $(".show").click(function() {
         $(this).next(".slidetoggle").slideToggle("slow", function() {
             // Animation complete.
@@ -396,10 +505,10 @@ $(document).ready(function() {
     });
 
     /*
-	 * Adding listeners for the facebook icon.
-	 */
+     * Adding listeners for search.
+     */
 
-    $("#facebook").click(function() {
+    $("#search").click(function() {
         var id = prompt("Enter your Device id:");
         var newID = {
             "deviceid": [id]
@@ -419,31 +528,38 @@ $(document).ready(function() {
                 getLamps();
                 return;
             }
-
-
         }
-
     });
 
-	/*
-	 * Adding listeners for....
-	 */
-	 
+    /*
+     * Adding listeners for....
+     */
+
     $(".show").click(function() {
         $(this).children("h3").children("i").toggleClass("fa-angle-right");
         $(this).children("h3").children("i").toggleClass("fa-angle-down");
     });
-    // var inputsForDayMode = getColorInputs(settings.daymode.lights);
-    // console.log(inputsForDayMode);
-    //
-    // $("#myform").append(inputsForDayMode);
-	
-	/*
-	 * Adding listeners for the form.
-	 */
-    $("#myform").submit(function(e) {
-        console.log(e.target.elements);
+    $("#nightMode").submit(function(e) {
+
         e.preventDefault();
         console.log("skickade formuläret");
+
+        // create new array from old settings
+        var newLampSettings = settings.nightMode.lights.map(function(light) {
+            var newHue = HSVtoHue(e.target.elements[light.id].jscolor.hsv);
+
+            light.sat = newHue.sat;
+            light.bri = newHue.bri;
+            light.hue = newHue.hue;
+
+            return light;
+        });
+
+        console.log(newLampSettings);
+
+        // update current settings with new lights
+        settings.nightMode.lights = newLampSettings;
+        // save settings to server
+        saveSettings(settings);
     });
 }); //end document ready
